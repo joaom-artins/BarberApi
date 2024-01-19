1fdb0fd8a72523fa3438568dc430949f99e8e7e9
Joao Martins <milanezijoao51@gmail.com>
2024-01-19 10:24:49 -0300

    first commit

---
+++ Model/Client.cs
@@ -0,0 +1,14 @@
+﻿namespace BarberAPI.Model
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        public Client() { }
+        public Client(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Model/HairCut.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BarberAPI.Model
+{
+    public class HairCut
+    {
+        public int Id { get; set; }
+        public int ClientId { get; set; }
+        [JsonIgnore]
+        public Client? Client { get; set; }
+        public DateTime ScheduledDate { get; set; }
+        public double Price { get; set; }
+        
+        public HairCut() { }
+
+        public HairCut(int clientId, DateTime scheduledDate, double price)
+        {
+            ClientId = clientId;
+            ScheduledDate = scheduledDate;
+            Price = price;
+        }
+    }
+}
